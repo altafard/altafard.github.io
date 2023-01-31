8e03389ea07d103892388ce110a63c68fdbf9efc
Alexander Pilipenko <altafard@gmail.com>
2023-01-31 14:27:50 +0000

    Release 0.0.22

---
+++ _next/static/EVbW2ZUpe_Hpbs-9zLnpZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/300-91c9dc522e6cad41.js","static/css/05636bbbea7128e9.css","static/chunks/pages/index-9d621bef7e13366e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
